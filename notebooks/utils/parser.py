5c4ab9e6d7f736046323e54596910002b6e09325
schwamic <schwarz.michael@posteo.net>
2024-07-05 08:33:02 +0200

    update

--- notebooks/utils/parser.py
+++ notebooks/utils/parser.py
@@ -42,8 +42,8 @@ class Parser:
         while True:
             try:
                 page_number = len(prompts)
-                base64_page=self.pdfToBase64(pdf_path, page_number)
-                image_url_prompt = {"type": "image_url","image_url": {"url": f"data:image/jpeg;base64,{base64_page[0]}","detail": "high"}}
+                base64_page, img_size = self.pdfToBase64(pdf_path, page_number)
+                image_url_prompt = {"type": "image_url","image_url": {"url": f"data:image/jpeg;base64,{base64_page}","detail": "high"}}
                 prompts.append(image_url_prompt)
             except Exception:
                 break
